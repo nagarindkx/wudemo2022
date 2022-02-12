ffe5f77ade2c78adc7bca17cd12652ff105d0d74
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2022-02-12 15:01:46 +0700

    init

---
+++ code/install.sh
@@ -0,0 +1,2 @@
+pip install django whitenoise psycopg2-binary python-dotenv django-crispy-forms
+django-admin startproject main
---
+++ rundeploy.sh
@@ -0,0 +1,11 @@
+# DEVELOPMENT
+
+# MAC/Linux
+#docker run -d -it -p 8080:8080 --name containername -v $PWD/code:/code python:3.8-slim
+# Windows
+#docker run -d -it -p 8080:8080 --name containername -v %CD%\code:/code python:3.8-slim
+
+# PRODUCTION
+#docker build -f Dockerfile.production -t containername:prod .
+docker tag containername:prod asia.gcr.io/gcloudprojectid/containername
+docker push asia.gcr.io/gcloudprojectid/containername
